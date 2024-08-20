fdf45507dc6766faa098a47181c0844d7a112853
Vinay Kumar Reddy Chethireddy <reddyvinay000@gmail.com>
2024-08-20 11:03:47 +0530

    dashboard

--- react_basic/src/Home.js
+++ react_basic/src/Home.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Home = () => {
   return (
-      <div>
-        Home
-      </div>
-  );
+    <div class="col-lg-12 mb-3">
+      <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 my-0 flex-column justify-content-center"> Dashboard </h1>
+    </div>
+      );
 }
 
 export default Home;
--- react_basic/src/Navbar.js
+++ react_basic/src/Navbar.js
@@ -3,16 +3,19 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <>
-    <div className="navbar">
+  <>
+  <div className="navbar">
     <div className="site-name" >
         <img src="https://intoneconnect.com/images/Logo.png" alt="Google" width="100" height="40"></img>
     </div>
     <div className="site-menu">
         <a href="#">About</a>
-        <a href="#">My Profile</a></div>
-</div>
-</>
+    </div>
+    <div className='site-menu'>
+      <a href="#">My Profile</a>
+    </div>
+  </div>
+  </>
   )
 };
 
